484da2ffcbdcc0251a0b8453b58bdcf2737d6282
Rene Ramos <rene_ramos@me.com>
2017-01-03 13:56:14 -0800

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+/**
+ * Created by reneramos on 1/3/17.
+ */
+import React from 'react'
+
+export default class Header extends React.Component {
+    render(){
+        return(
+            <h1>Header</h1>
+        )
+    }
+}
