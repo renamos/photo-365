82936f8bfb5dd3b6309485747f61356058116e88
Rene Ramos <rene_ramos@me.com>
2017-01-31 11:40:24 -0800

    updated get request URLs

--- src/actions/get-image.js
+++ src/actions/get-image.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export function getImage(){
-    var request = axios.get('http://localhost:8000/images/last-image');
+    var request = axios.get('http://everyday-ramos-api.herokuapp.com/images/last-image');
     return {type:'GET_IMAGE', payload:request}
 }
--- src/actions/photos.js
+++ src/actions/photos.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export function recentImages(){
-    var request = axios.get('http://localhost:8000/images/recent');
+    var request = axios.get('http://everyday-ramos-api.herokuapp.com/images/recent');
     return {type:'GET_RECENT_IMAGES', payload:request}
 }
